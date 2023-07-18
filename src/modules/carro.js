378e98dc5fa91a2f958ceb918e9b9131ceed4f88
Gabriel França <gabrielfranca5592@gmail.com>
2023-07-17 21:19:49 -0300

    Teste

---
+++ src/modules/carro.js
@@ -0,0 +1,13 @@
+export class Carro {
+    // atributos
+    marca;
+    modelo;
+    ano;
+    // constructor
+    constructor(modelo, marca, ano){
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+    }
+    // métodos
+}
